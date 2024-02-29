72d5bb63b2fb941b31332bc254d2e110803c7483
kiko <kt1767@gmail.com>
2024-03-01 05:14:33 +0530

    Add navigation and screens to App.js

---
+++ App.js
@@ -0,0 +1,83 @@
+import 'react-native-gesture-handler';
+import {
+  NavigationContainer,
+  useNavigation,
+  DrawerActions,
+} from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Entypo';
+import DrawerContent from './DrawerContent';
+
+
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+
+const StackNav = () => {
+  const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        statusBarColor: '#0163d2',
+        headerStyle: {
+          backgroundColor: '#0163d2',
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerLeft: () => {
+            return (
+              <Icon
+                name="menu"
+                onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+                size={30}
+                color="#fff"
+              />
+            );
+          },
+        }}
+      />
+
+    </Stack.Navigator>
+  );
+};
+
+const DrawerNav = () => {
+  const Drawer = createDrawerNavigator();
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <DrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Drawer.Screen name="HomeDrawer" component={StackNav} />
+    </Drawer.Navigator>
+  );
+};
+function App() {
+
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={DrawerNav} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
